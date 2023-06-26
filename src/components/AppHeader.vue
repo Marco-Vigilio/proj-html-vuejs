4208098162be0f476b58ee34900bda20b3f4e667
Marco-Vigilio <tonyvigilio@gmail.com>
2023-06-26 12:30:16 +0200

    inserisco un div.contain per dare una grandezza generica a tutto il campo header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,26 +1,27 @@
 <template>
     <header>
-        <nav class="contain">
-            <div class="image-logo">
-                logo
-            </div>
+        <div class="contain">
+            <nav>
+                <div class="image-logo">
+                    logo
+                </div>
 
-            <div class="icons">
-                <ul>
-                    <li>icone</li>
-                </ul>
-            </div>
-        </nav>
+                <div class="icons">
+                    <ul>
+                        <li>icone</li>
+                    </ul>
+                </div>
+            </nav>
 
-        <div class="jumbotron contain">
-            <div class="spot">
-                qualcosa
-            </div>
-            <div class="my_image">
-                immagine
+            <div class="jumbotron">
+                <div class="spot">
+                    qualcosa
+                </div>
+                <div class="my_image">
+                    immagine
+                </div>
             </div>
         </div>
-
     </header>
 </template>
 <script>
@@ -32,41 +33,43 @@ export default {
 header {
     background-color: gold;
 
-    nav.contain {
+    div.contain {
         max-width: 1180px;
         margin: 0 auto;
-        display: flex;
-        justify-content: space-between;
 
-        .icons {
-            background-color: violet;
+        nav {
             display: flex;
-            justify-content: flex-end;
+            justify-content: space-between;
+
+            .icons {
+                background-color: violet;
+                display: flex;
+                justify-content: flex-end;
+            }
+        }
+
+        .jumbotron {
+            display: flex;
+
+            div.my_image,
+            div.spot {
+                width: calc(100% / 2);
+                height: 300px;
+            }
+
+            .spot {
+                background-color: red;
+                border: 2px solid black;
+            }
+
+            .my_image {
+                background-color: green;
+                border: 2px solid black;
+            }
+
         }
     }
 
-    div .jumbotron,
-    .contain {
-        max-width: 1180px;
-        margin: 0 auto;
-        display: flex;
 
-        div.my_image,
-        div.spot {
-            width: calc(100% / 2);
-            height: 300px;
-        }
-
-        .spot {
-            background-color: red;
-            border: 2px solid black;
-        }
-
-        .my_image {
-            background-color: green;
-            border: 2px solid black;
-        }
-
-    }
 }
 </style>
