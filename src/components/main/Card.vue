616372859c3818ff16edf54aca62cbc912f1506d
Marco-Vigilio <tonyvigilio@gmail.com>
2023-06-26 15:02:02 +0200

    creo struttura per Courses e aggiungo component Card

---
+++ src/components/main/Card.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card">
+        <div class="header">immagine</div>
+        <div class="main">Descrizione + prezzo</div>
+        <div class="footer">N.Lezioni + N.studenti</div>
+    </div>
+
+    <div class="card">
+        <div class="header">immagine</div>
+        <div class="main">Descrizione + prezzo</div>
+        <div class="footer">N.Lezioni + N.studenti</div>
+    </div>
+
+    <div class="card">
+        <div class="header">immagine</div>
+        <div class="main">Descrizione + prezzo</div>
+        <div class="footer">N.Lezioni + N.studenti</div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Card",
+}
+</script>
+<style lang="scss" scoped>
+.card {
+    width: calc(100% / 3);
+}
+</style>
--- src/components/main/Courses.vue
+++ src/components/main/Courses.vue
@@ -1,9 +1,34 @@
 <template>
-    <section>Corsi</section>
+    <section>
+        <div class="title">Titolo</div>
+        <div class="cards">
+            <Card />
+        </div>
+        <div class="button">
+            <button class="button">Vedi tutti i corsi</button>
+        </div>
+    </section>
 </template>
 <script>
+import Card from './Card.vue';
 export default {
     name: "Courses",
+    components: {
+        Card,
+    }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+div.title {
+    text-align: center;
+}
+
+.cards {
+    display: flex;
+}
+
+.button {
+    display: flex;
+    justify-content: center;
+}
+</style>
