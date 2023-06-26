a566ec6a59944ee7d8d899e9ca8d29bfad2a8a0c
Marco-Vigilio <tonyvigilio@gmail.com>
2023-06-26 21:10:20 +0200

    faccio vedere le mie Cards dati in un array (non ancora strutturato Card)

--- src/components/main/Card.vue
+++ src/components/main/Card.vue
@@ -1,29 +1,69 @@
 <template>
     <div class="card">
-        <div class="header">immagine</div>
-        <div class="main">Descrizione + prezzo</div>
-        <div class="footer">N.Lezioni + N.studenti</div>
-    </div>
+        <div class="header">
+            <img :src="image" alt="image of couse">
+        </div>
+        <div class="main">
+            <span>&dollar;{{ priceNumber }}</span>
+            <span>.{{ priceNumberDecimal }}</span>
+            <p>{{ name }}</p>
+        </div>
+        <div class="footer">
+            <span v-if="numLesson === '1'">
+                <i class="fa-regular fa-file-lines"></i>
+                {{ numLesson }} Lesson
+            </span>
+            <span v-else>
+                <i class="fa-regular fa-file-lines"></i>
+                {{ numLesson }} Lessons
+            </span>
 
-    <div class="card">
-        <div class="header">immagine</div>
-        <div class="main">Descrizione + prezzo</div>
-        <div class="footer">N.Lezioni + N.studenti</div>
-    </div>
-
-    <div class="card">
-        <div class="header">immagine</div>
-        <div class="main">Descrizione + prezzo</div>
-        <div class="footer">N.Lezioni + N.studenti</div>
+            <span>
+                <i class="fa-regular fa-user"></i>
+                {{ numStudents }} Students
+            </span>
+        </div>
     </div>
 </template>
 <script>
 export default {
     name: "Card",
+    props: {
+        name: String,
+        priceNumber: String,
+        priceNumberDecimal: String,
+        image: String,
+        numLesson: String,
+        numStudents: String,
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        price() {
+            console.log(this.name);
+        }
+    },
+    /*
+    mounted() {
+        this.price(price);
+    },
+    */
 }
 </script>
 <style lang="scss" scoped>
 .card {
-    width: calc(100% / 3);
+    width: (calc(100% / 3)-2);
+    border: 2px solid black;
+
+    header {
+        width: 100%;
+
+        img {
+            width: 100%;
+        }
+    }
 }
 </style>
--- src/components/main/Courses.vue
+++ src/components/main/Courses.vue
@@ -5,7 +5,8 @@
             <h2>Latest Online Courses</h2>
         </div>
         <div class="cards">
-            <Card />
+            <Card v-for="course in arrayCourses" :name=course.name :image=course.image :numLesson=course.numberLesson
+                :numStudents=course.students :priceNumber=course.price[0] :priceNumberDecimal=course.price[1] />
         </div>
         <div class="button">
             <button class="button">Vedi tutti i corsi</button>
@@ -18,28 +19,81 @@ export default {
     name: "Courses",
     components: {
         Card,
-    }
+    },
+    data() {
+        return {
+            arrayCourses: [
+                {
+                    name: "How to be Successful: Create A Growth Mindset For Success",
+                    numberLesson: "3",
+                    students: "50",
+                    price: ["30", "00"],
+                    image: "../../../src/assets/img/motivation-course-06-480x298.jpg",
+                },
+                {
+                    name: "How to Build Confidence in Your Abilities",
+                    numberLesson: "1",
+                    students: "50",
+                    price: ["30", "00"],
+                    image: "../../../src/assets/img/motivation-course-05-480x298.jpg",
+                },
+                {
+                    name: "Productivity Machine – Focus in a Distracted World",
+                    numberLesson: "5",
+                    students: "50",
+                    price: ["20", "00"],
+                    image: "../../../src/assets/img/motivation-course-04-480x298.jpg",
+                },
+                {
+                    name: "Effective Time Management Mastery – Complete Guide",
+                    numberLesson: "18",
+                    students: "50",
+                    price: ["20", "00"],
+                    image: "../../../src/assets/img/motivation-course-03-480x298.jpg",
+                },
+                {
+                    name: "Body Language Secrets for Entrepreneurs",
+                    numberLesson: "19",
+                    students: "50",
+                    price: ["25", "99"],
+                    image: "../../../src/assets/img/motivation-course-02-480x298.jpg",
+                },
+                {
+                    name: "Management Skills: The Science of Leadership",
+                    numberLesson: "17",
+                    students: "50",
+                    price: ["19", "99"],
+                    image: "../../../src/assets/img/motivation-course-01-480x298.jpg",
+                },
+            ],
+        }
+    },
 }
 </script>
 <style lang="scss" scoped>
-div.title {
-    text-align: center;
-    padding-bottom: 2rem;
+section {
+    div.title {
+        text-align: center;
+        padding-bottom: 2rem;
 
-    h2 {
-        font-size: 2.5rem;
+        h2 {
+            font-size: 2.5rem;
+        }
     }
-}
 
-.cards {
-    display: flex;
-}
+    .cards {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+    }
 
-.button {
-    display: flex;
-    justify-content: center;
-    border: none;
-    padding: 1rem 4rem;
-    border-radius: .3rem;
+    .button {
+        display: flex;
+        justify-content: center;
+        border: none;
+        margin-top: 1rem;
+        padding: 1rem 4rem;
+        border-radius: .3rem;
+    }
 }
 </style>
