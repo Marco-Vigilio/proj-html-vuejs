79b7909bf9667c6a5150a208480b48404e925605
Marco-Vigilio <tonyvigilio@gmail.com>
2023-06-26 14:41:59 +0200

    creo struttura per About

--- src/components/main/AboutMe.vue
+++ src/components/main/AboutMe.vue
@@ -1,9 +1,43 @@
 <template>
-    <section>About me</section>
+    <section>
+        <div class="introduction">
+            <article>About Me</article>
+            <article>Articolo</article>
+        </div>
+        <div class="about">
+            <article class="video">Video</article>
+            <article>qualcosa</article>
+        </div>
+    </section>
 </template>
 <script>
 export default {
     name: "About Me",
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+section {
+    .introduction {
+        display: flex;
+
+        article {
+            border: 2px solid black;
+            background-color: red;
+            width: calc(100% / 2);
+            display: flex;
+            justify-content: center;
+        }
+    }
+
+    .about {
+        display: flex;
+
+        article.video {
+            background-color: violet;
+            width: 60%;
+        }
+
+        //no margin, PADDING
+    }
+}
+</style>
