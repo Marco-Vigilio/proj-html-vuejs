9cbbd7b750965e25a4e6efa9a46d8c689df4f5b0
Marco-Vigilio <tonyvigilio@gmail.com>
2023-06-26 15:46:50 +0200

    creo struttura per component Blog

--- src/components/main/Blog.vue
+++ src/components/main/Blog.vue
@@ -1,9 +1,29 @@
 <template>
-    <section>Blog</section>
+    <section>
+        <span>Titilino</span>
+        <h1>Titolo</h1>
+        <div class="items">
+            <div>Item</div>
+            <div>Item</div>
+            <div>Item</div>
+            <div>Item</div>
+        </div>
+        <a href="#">Blogs</a>
+    </section>
 </template>
 <script>
 export default {
     name: "Blog",
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.items {
+    display: flex;
+    justify-content: space-between;
+}
+
+a {
+    display: flex;
+    justify-content: center;
+}
+</style>
