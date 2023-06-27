8ca83f3f4515764d4e48b42c956351aa1909c2fe
Marco-Vigilio <tonyvigilio@gmail.com>
2023-06-27 11:50:12 +0200

    dispongo in maniera ordinata la lista dei Widget

--- src/components/footer/Widget.vue
+++ src/components/footer/Widget.vue
@@ -4,19 +4,23 @@
             <h2>Explore</h2>
             <div class="list">
                 <ul>
-                    <li v-for="(element, index) in arrayList">
-                        {{ index }}
+                    <li v-for="(element, index) in arrayExplore1">
+                        {{ index }} {{ element }}
                     </li>
                 </ul>
                 <ul>
-                    <li>Qualcosa</li>
+                    <li v-for="(element, index) in arrayExplore2">
+                        {{ index }} {{ element }}
+                    </li>
                 </ul>
             </div>
         </div>
         <div class="information">
             <h2>information</h2>
             <ul>
-                <li>Qualcosa</li>
+                <li v-for="(element, index) in arrayInformation">
+                    {{ element }}
+                </li>
             </ul>
         </div>
     </section>
@@ -29,10 +33,33 @@ export default {
     },
     data() {
         return {
-            arrayExplore: [],
+            arrayExplore1: [],
+            arrayExplore2: [],
             arrayInformation: [],
         }
-    }
+    },
+    methods: {
+        fillArray() {
+            this.arrayExplore1 = (this.arrayList.slice(0, 3)); //inizia da 0 e prende 3 elementi
+            this.arrayExplore2 = (this.arrayList.slice(3, 6));
+            this.arrayInformation = (this.arrayList.slice(6, this.arrayList.length));
+            /*
+            for (let index = 0; index < 3; index++) {
+                this.arrayExplore1 = this.arrayList[index];
+            }
+
+            for (let index = 3; index < 6; index++) {
+                this.arrayExplore2 = this.arrayList[index];
+            }
+            for (let index = 6; index < this.arrayList.length; index++) {
+                this.arrayInformation = this.arrayList[index];
+            }
+            */
+        }
+    },
+    mounted() {
+        this.fillArray();
+    },
 }
 </script>
 <style lang="scss" scoped>
