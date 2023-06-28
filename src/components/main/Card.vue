f270aaad532c988f412eae9fa3103ef2ef7f4851
Marco-Vigilio <tonyvigilio@gmail.com>
2023-06-28 17:26:23 +0200

    inserisco shadow e scale alle Card

--- src/components/main/Card.vue
+++ src/components/main/Card.vue
@@ -1,30 +1,32 @@
 <template>
     <div class="card">
-        <div class="header">
-            <img :src="image" alt="image of couse">
-        </div>
-        <div class="contain">
-            <div class="main">
-                <span class="green">&dollar;{{ priceNumber }}</span>
-                <span class="green">.{{ priceNumberDecimal }}</span>
-                <p>{{ name }}</p>
+        <a href="#">
+            <div class="header">
+                <img :src="image" alt="image of couse">
             </div>
-            <div class="footer">
-                <span v-if="numLesson === '1'">
-                    <i class="fa-regular fa-file-lines"></i>
-                    {{ numLesson }} Lesson
-                </span>
-                <span v-else>
-                    <i class="fa-regular fa-file-lines"></i>
-                    {{ numLesson }} Lessons
-                </span>
+            <div class="contain">
+                <div class="main">
+                    <span class="green">&dollar;{{ priceNumber }}</span>
+                    <span class="green">.{{ priceNumberDecimal }}</span>
+                    <p>{{ name }}</p>
+                </div>
+                <div class="footer">
+                    <span v-if="numLesson === 1">
+                        <i class="fa-regular fa-file-lines"></i>
+                        {{ numLesson }} Lesson
+                    </span>
+                    <span v-else>
+                        <i class="fa-regular fa-file-lines"></i>
+                        {{ numLesson }} Lessons
+                    </span>
 
-                <span>
-                    <i class="fa-regular fa-user"></i>
-                    {{ numStudents }} Students
-                </span>
+                    <span>
+                        <i class="fa-regular fa-user"></i>
+                        {{ numStudents }} Students
+                    </span>
+                </div>
             </div>
-        </div>
+        </a>
     </div>
 </template>
 <script>
@@ -57,6 +59,7 @@ export default {
 </script>
 <style lang="scss" scoped>
 @use '../../style/partilas/variables' as*;
+@use '../../style/partilas/maxins' as*;
 
 span.green {
     color: $green;
@@ -66,36 +69,49 @@ span.green {
     width: (calc((100% - 4rem) / 3));
     border-radius: .4rem;
     overflow: hidden;
+    @include shadow;
 
-    .contain {
-        padding: 1rem 2rem;
+    .header {
+        width: 100%;
+        overflow: hidden;
+        /*
+        img {
+            transition: 1s;
+            transform: scale(1.1);
+        }
+*/
+    }
 
-        .header {
-            width: 100%;
 
-            img {
-                width: 100%;
-                display: block;
-            }
+}
+
+.card .header img {
+    transition: 1s;
+}
+
+.card:hover .header img {
+    transform: scale(1.1);
+}
+
+.contain {
+    padding: 1rem 2rem;
+
+    .main {
+        span:first-child {
+            font-size: 1.5rem;
         }
 
-        .main {
-            span:first-child {
-                font-size: 1.5rem;
-            }
-
-            p {
-                font-size: 1.4rem;
-                padding: 1rem 0;
-            }
+        p {
+            font-size: 1.4rem;
+            padding: 1rem 0;
         }
+    }
 
-        .footer {
-            padding: 1.5rem 0;
+    .footer {
+        padding: 1.5rem 0;
 
-            span:last-child {
-                padding-left: 1.5rem;
-            }
+        span:last-child {
+            padding-left: 1.5rem;
         }
     }
 }
